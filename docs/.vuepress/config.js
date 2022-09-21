660f3196124721d8e8ee1f4c1756b8c0d43827c3
YouXinxuexi <113845588+YouXinxuexi@users.noreply.github.com>
2022-09-21 11:13:05 +0800

    Create config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,17 @@
-const head = require('./config/head.js');
-const plugins = require('./config/plugins.js');
-const themeConfig = require('./config/themeConfig.js');
-
-module.exports = {
-  theme: 'vdoing', // 使用npm包主题
-  title: "vdoing-template",
-  description: 'vdoing博客主题模板',
-  base: '/vdoing-template/', // 格式：'/<仓库名>/'， 默认'/'
-  markdown: {
-    lineNumbers: true, // 代码行号
-  },
-
-  head,
-  plugins,
-  themeConfig,
-}
+// vssue 评论插件
+  plugins: [
+    [
+      "vuepress-plugin-vssue-global",
+      {
+        platform: "github",
+        title: "[Comment]<%- frontmatter.title %>",
+        needComments: true,
+        // 其他的 Vssue 配置
+        autoCreateIssue: true,
+        clientId: "d3ec4ca6363950ca41a2",
+        clientSecret: "897465b6393f1d663e6128d2fab6959a0c0333cc",
+        owner: "lql95",
+        repo: "lql-notes",
+      },
+    ],
+  ],
